8eefd8732126d3e35f94a608814b2cb80b7f7490
Ben Parish <ben@onshop.co.uk>
2013-02-11 14:35:47 +0000

    #49 Convert TEITags extension to work with MW 1.20.2

--- w/extensions/TEITags/TEITags.php
+++ w/extensions/TEITags/TEITags.php
@@ -1,154 +1,56 @@
 <?php
 
 /**
- * Copyright (C) 2010 Richard Davis
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License Version 2, as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- * @package MediaWiki
- * @subpackage Extensions
- * @author Richard Davis <r.davis@ulcc.ac.uk>
- * @copyright 2010 Richard Davis
- */
+ * Copyright (C) 2013 Richard Davis
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License Version 2, as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+* @package MediaWiki
+* @subpackage Extensions
+* @author Richard Davis <r.davis@ulcc.ac.uk>
+* @author Ben Parish <b.parish@ulcc.ac.uk>
+* @copyright 2013 Richard Davis
+*/
 
-if (!defined('MEDIAWIKI')) die();
-
-$wgExtensionFunctions[] = "3";
+# Alert the user that this is not a valid entry point to MediaWiki if they try to access the special pages file directly.
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo <<<EOT
+To install my extension, put the following line in LocalSettings.php:
+require_once( "\$IP/extensions/JBTEIToolbar/JBTEIToolbar.php" );
+EOT;
+	exit( 1 );
+}
 
 $wgExtensionCredits['specialpage'][] = array(
-					     'name'    => 'TEI-Tags',
-					     'author'  => 'Richard Davis',
-					     'version' => '0.1',
-        'url'     => 'http://dablog.ulcc.ac.uk/'
-					     );
+	'path'        =>  __FILE__,
+	'name'        => new Message( 'tei-tags' ),
+    'type'		  => 'parserhook',
+	'author'      => 'Richard Davis',
+	'url'         => 'http://dablog.ulcc.ac.uk/',
+	'version'     => '0.2',
+	'description' => new Message( 'teitags-descr' )
+);
 
-/* TEI tags */
-$wgExtensionFunctions[] = 'wfTEITags';
-$wgExtensionCredits['parserhook'][] = array(
-					    'name'    => 'TEI-Tags',
-					    'author'  => 'Richard Davis',
-					    'version' => '0.1',
-        'url'     => 'http://dablog.ulcc.ac.uk/'
-					    );
-
-function wfTEITags() {
-  global $wgParser;
-  $wgParser->setHook( "tei", "wfRenderTEI" );
-  $wgParser->setHook( "lb", "wfRenderLB" );
-  $wgParser->setHook( "pb", "wfRenderPB" );
-  $wgParser->setHook( "del", "wfRenderDEL" );
-  $wgParser->setHook( "add", "wfRenderADD" );
-  $wgParser->setHook( "gap", "wfRenderGAP" );
-  $wgParser->setHook( "unclear", "wfRenderUNCLEAR" );
-  $wgParser->setHook( "note", "wfRenderNOTE" );
-  $wgParser->setHook( "hi", "wfRenderHI" );
-  $wgParser->setHook( "head", "wfRenderHEAD" );
-  $wgParser->setHook( "sic", "wfRenderSIC" );
-  $wgParser->setHook( "foreign", "wfRenderFOREIGN" );
-}
-
-function wfRenderTEI( $input, $args, $parser, $frame ) {
-  $output = $parser->recursiveTagParse( $input, $frame );
-  return "<span class='tei-tei'>" .
-    htmlspecialchars( $output ) .
-    "</span>";
-}
-function wfRenderLB() {
-  return "<br/>";
-}
-function wfRenderPB() {
-  return "<br/>---<em>page break</em>---<br/>";
-}
-function wfRenderDEL( $input, $args, $parser, $frame=FALSE ) {
-  $output = $parser->recursiveTagParse( $input, $frame );
-  return "<span class='tei-del'>" .
-    htmlspecialchars( $output ) .
-    "</span>";
-}
-function wfRenderADD( $input, $args, $parser, $frame=FALSE ) {
-  $output = $parser->recursiveTagParse( $input, $frame );
-  return "<span class='tei-add'>" .
-    htmlspecialchars( $output ) .
-    "</span>";
-}
-function wfRenderUNCLEAR( $input, $args, $parser, $frame=FALSE ) {
-  $output = $parser->recursiveTagParse( $input, $frame );
-  return "<span class='tei-unclear'>" .
-    htmlspecialchars( $output ) .
-    "</span>";
-}
-function wfRenderNOTE( $input, $args, $parser, $frame=FALSE ) {
-  $output = $parser->recursiveTagParse( $input, $frame );
-  return "<span class='tei-note'>" .
-    htmlspecialchars( $output ) .
-    "</span>";
-}
-
-function wfRenderHI( $input, $args, $parser, $frame=FALSE ) {
-  $render = $args['rend'];
-  $output = $parser->recursiveTagParse( $input, $frame );
-  return "<span class='tei-hi $render'>" .
-    htmlspecialchars( $output ) .
-    "</span>";
-}
-
-function wfRenderHEAD( $input, $args, $parser, $frame=FALSE ) {
-  $output = $parser->recursiveTagParse( $input, $frame );
-  return "<span class='tei-head'>" .
-    htmlspecialchars( $output ) .
-    "</span>";
-}
-
-function wfRenderFOREIGN( $input, $args, $parser, $frame=FALSE ) {
-  $output = $parser->recursiveTagParse( $input, $frame );
-  return "<span class='tei-foreign'>" .
-    htmlspecialchars( $output ) .
-    "</span>";
-}
-
-function wfRenderSIC( $input, $args, $parser, $frame=FALSE ) {
-  $output = $parser->recursiveTagParse( $input, $frame );
-  return "<span class='tei-sic'>" .
-    htmlspecialchars( $output ) .
-    "</span>";
-}
-
-function wfRenderGAP() {
-  return "<span class='tei-gap'>" .
-    "&nbsp;" .
-    "</span>";
-}
-
-/* show list of tags  */
-function wfTEITagsSpecialpage() {
-
-  class TEITags extends SpecialPage {
-
-    function TEITags() {
-      SpecialPage::SpecialPage( 'TEITags' );
-      $this->includable(true);
-      $this->listed(false);
-    }
-
-    function execute( $subPage ) {
-      global $wgOut, $wgRequest;
+$wgExtensionMessagesFiles[ 'TEITags' ] = __DIR__ . '/TEITags.i18n.php';
+$wgAutoloadClasses[ 'SpecialTEITags' ] = __DIR__ . '/SpecialTEITags.php';
+require_once __DIR__ . '/TEITags.body.php';
 
 
-      $text = "some text";
+$wgSpecialPages[ 'TEITags' ] = 'SpecialTEITags';
 
-      exit;
-    }
-  } /* class */
-  SpecialPage::addPage( new TEITags );
-}
+$TEITags  = new TEITags;
+
+$wgHooks['ParserFirstCallInit'][] = array( $TEITags, 'SetHooks' );
+
+
---
+++ w/extensions/TEITags/TEItags.i18n.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Copyright (C) 2013 Richard Davis
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License Version 2, as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+* Internationalisation for TEITags extension
+*
+* @package MediaWiki
+* @subpackage Extensions
+* @author Richard Davis <r.davis@ulcc.ac.uk>
+* @author Ben Parish <b.parish@ulcc.ac.uk>
+* @copyright 2013 Richard Davis
+*/
+
+$messages = array();
+
+$messages['en'] = array(  'tei-tags'      => 'TEI-Tags'
+						 ,'teitags-descr' => 'Adds support for TEI (Text Encoding Initiative) tags'
+					   );
