3742200200dd21e3c34fb68d9b83d6d3b0d45dfd
Ben Parish <ben@onshop.co.uk>
2013-04-12 11:31:31 +0100

    Fixing alignments

--- scripts/Importer/config.ini.php
+++ scripts/Importer/config.ini.php
@@ -3,14 +3,14 @@
 ;/*
 
 [common]
-database.adapter         = Pdo_Mysql
-database.params.host     = localhost
-site_name				 = 'Transcribe Bentham Transcription Desk'
-home_page_name			 = Transcribe_Bentham
-page_path				 = 'td';
-page_prefix              = 'JB';
-version					 = 1.20.2
-export_version			 = 0.7
+database.adapter      = Pdo_Mysql
+database.params.host  = localhost
+site_name             = 'Transcribe Bentham Transcription Desk'
+home_page_name        = Transcribe_Bentham
+page_path             = 'td';
+page_prefix           = 'JB';
+version               = 1.20.2
+export_version        = 0.7
 
 [production : common]
 
@@ -18,18 +18,18 @@ export_version			 = 0.7
 
 
 [development : common]
-webhost                  = cbp-transcription-desk.local
-admin.email				 =
-path.slicer              = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\scripts\Slicer\slicer.pl'
-path.mw.importer		 = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\w\maintenance\importDump.php'
-path.csv.import          = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\scripts\tests\Files\input\text\Bentham-Image-Metatada-Table.txt'
-path.image.import		 = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\scripts\tests\Files\input\images'
-path.image.export		 = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\zimages'
-;path.image.export		 = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\scripts\tests\Files\output\images'
-path.xml.export          = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\scripts\tests\Files\output\text'
-path.logs                = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\scripts\tests\Files\logs'
-path.archive             = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\scripts\tests\Files\archive'
-system.group             =
+webhost             = cbp-transcription-desk.local
+admin.email         =
+path.slicer         = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\scripts\Slicer\slicer.pl'
+path.mw.importer    = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\w\maintenance\importDump.php'
+path.csv.import     = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\scripts\tests\Files\input\text\Bentham-Image-Metatada-Table.txt'
+path.image.import   = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\scripts\tests\Files\input\images'
+path.image.export   = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\zimages'
+;path.image.export  = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\scripts\tests\Files\output\images'
+path.xml.export     = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\scripts\tests\Files\output\text'
+path.logs           = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\scripts\tests\Files\logs'
+path.archive        = 'M:\Dropbox\web\sites\cbp-transcription-desk.local\scripts\tests\Files\archive'
+system.group        =
 
 database.params.username = transcription
 database.params.password = transcription
