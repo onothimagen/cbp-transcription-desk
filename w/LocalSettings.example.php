c014f65d49dbac871cf3de4a1a1f2a2dc8f05bed
Ben Parish <ben@onshop.co.uk>
2013-04-16 17:36:54 +0100

    Revised and rationalised to be simpler

--- w/LocalSettings.example.php
+++ w/LocalSettings.example.php
@@ -1,8 +1,32 @@
-#############################################################
+<?php
+
+/**
+ * Copyright (C) 2013 Richard Davis
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License Version 2, as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+* Append this to then end of LocalSettings.php
+*
+* @package MediaWiki
+* @author Ben Parish <b.parish@ulcc.ac.uk>
+* @author Richard Davis <r.davis@ulcc.ac.uk>
+*/
+
 #############################################################
 # CUSTOMISATION SETTINGS
 #############################################################
-#############################################################
+
 
 # ENABLING PRETTY URLS (N.B HTACCESS ALSO USED
 $wgArticlePath = "/td/$1";
@@ -12,6 +36,8 @@ $wgUsePathInfo = true;        # Enable use of pretty URLs
 # BP Enabled by default. We need to switch this off because the bentham modern template is not HTML 5
 # HTML Tidy will complain that script tags do not contain a 'type' attribute Html->inlineScript()
 $wgHtml5 = false;
+
+$wgDefaultSkin = "cbptranscription";
 
 #############################################################
 # USER PERMISSIONS
@@ -30,7 +56,7 @@ $wgAutopromote[ 'emailconfirmed' ]                = APCOND_EMAILCONFIRMED;
 $wgImplicitGroups[]                               = 'emailconfirmed';
 
 # Finally, set it to true for the desired group.
-$wgGroupPermissions[ 'emailconfirmed' ][ 'edit' ] = true;
+$wgGroupPermissions[ 'emailconfirmed' ][ 'edit' ]     = true;
 
 #############################################################
 # DEFINE NAMESPACES AND PROTECT FROM EDITING
@@ -43,8 +69,6 @@ define("NS_METADATA_TALK", 101);
 $wgExtraNamespaces[NS_METADATA]      = 'Metadata';
 $wgExtraNamespaces[NS_METADATA_TALK] = 'Metadata_talk';
 $wgNamespaceProtection[NS_METADATA]  = array( 'editmetadata' ); #permission "editmetaata" required to edit the Metadata namespace
-
-$wgGroupPermissions['sysop']['editmetadata'] = true;      #permission "editmetadata" granted to users in the "sysop" group
 
 #Set default searching
 $wgNamespacesToBeSearchedDefault = array(
@@ -71,114 +95,30 @@ $wgNamespacesToBeSearchedDefault = array(
 $wgNamespaceProtection[ NS_HELP ]    = array( 'edithelp' );
 $wgNamespacesWithSubpages[ NS_HELP ] = true;
 
-$wgNamespaceProtection[NS_HELP] = array('edithelp');
-$wgNamespacesWithSubpages[NS_HELP] = true;
+$wgNamespaceProtection[NS_HELP]      = array('edithelp');
+$wgNamespacesWithSubpages[NS_HELP]   = true;
 
-$wgNamespaceProtection[104]     = array('edittype');
-$wgNamespacesWithSubpages[104]  = true;
+$wgNamespaceProtection[104]          = array('edittype');
+$wgNamespacesWithSubpages[104]       = true;
 
-$wgNamespaceProtection[102]     = array('editproperty');
-$wgNamespacesWithSubpages[102]  = true;
+$wgNamespaceProtection[102]          = array('editproperty');
+$wgNamespacesWithSubpages[102]       = true;
 
+$wgGroupPermissions[ '*' ]             [ 'edittype' ]    = false;
+$wgGroupPermissions[ '*' ]             [ 'edithelp' ]    = false;
+$wgGroupPermissions[ 'sysop' ]         [ 'edittype' ]    = true;
+$wgGroupPermissions[ 'sysop' ]         [ 'edithelp' ]    = true;
+$wgGroupPermissions[ 'user' ]          [ 'upload' ]      = true;
+$wgGroupPermissions['sysop' ]          [ 'editmetadata'] = true;      #permission "editmetadata" granted to users in the "sysop" group
 
-#############################################################
-# CUSTOM FILE PATHS SETUP
-#############################################################
+##############################################################
+# CBP TRANSCRIPTION EXTENSIONS
+##############################################################
 
-$root_path 	= 'Path to your virtual host';
+$root_path 	= 'Path to your virtual host root';
 
 $transcription_extensions_path 	= $root_path . 'w/extensions/';
-$transcription_includes_path 	= $root_path . 'w/includes/';
-$transcription_skins_path		= $root_path . 'w/skins/';
 
-$path = array( $transcription_extensions_path
-			 , $transcription_includes_path
-		     , $transcription_skins_path );
-
-set_include_path( implode( PATH_SEPARATOR, $path ) . PATH_SEPARATOR . get_include_path() );
-
-#############################################################
-# EXTENSIONS
-#############################################################
-
-############################
-# Off the shelf
-############################
-
-require_once( 'awc/forums/awc_forum.php' );
-
-require_once( 'DiscussionThreading/DiscussionThreading.php' );
-require_once( 'MyVariables/MyVariables.php' );
-require_once( 'NoTitle/NoTitle.php' );
-require_once( 'Progressbar/Progressbar.php' );
-require_once( 'VideoFlash/VideoFlash.php' );
-require_once( 'RSSReader/RSSReader.php' );
-require_once( 'SocialProfile/SocialProfile.php' );
-
-
-#require_once( 'WikiEditor/WikiEditor.php' );
-
-#Enables use of WikiEditor by default but still allow users to disable it in preferences
-#$wgDefaultUserOptions['usebetatoolbar'] = 1;
-
-
-##########################################
-# ConfirmEdit Extension
-##########################################
-
-require_once( 'ConfirmEdit/ConfirmEdit.php' );
-require_once( 'ConfirmEdit/ReCaptcha.php' );
-$wgCaptchaClass = 'ReCaptcha';
-
-$wgReCaptchaPublicKey = '';
-$wgReCaptchaPrivateKey = '';
-
-#########################################################
-# Configure Extension (replaces GroupPermissionsManager)
-#########################################################
-
-require_once( 'Configure/Configure.php' );
-efConfigureSetup();
-
-// Array of editable settings. If this is a non-empty array only the settings in this array will be allowed to be modified
-$wgConfigureEditableSettings = array();
-
-$wgGroupPermissions[ '*' ]     [ 'edittype' ]		= false;
-$wgGroupPermissions[ '*' ]     [ 'edithelp' ]		= false;
-$wgGroupPermissions[ 'sysop' ] [ 'edittype' ]		= true;
-$wgGroupPermissions[ 'sysop' ] [ 'edithelp' ]		= true;
-$wgGroupPermissions[ 'user' ]  [ 'upload' ]  		= true;
-
-############################
-# UserMerge/delete Extension
-############################
-
-require_once( 'UserMerge/UserMerge.php' );
-$wgGroupPermissions[ 'bureaucrat' ]	[ 'usermerge' ]  	= true;
-#optional - default is array( 'sysop' )
-$wgUserMergeProtectedGroups = array( 'sysop' );
-
-
-##########################################
-# SemanticWiki Extension and dependencies
-##########################################
-
-# DataValues
-require_once( 'DataValues/DataValues.php' );
-require_once( 'Validator/Validator.php' );
-
-require_once( 'SemanticMediaWiki/SemanticMediaWiki.php' );
-enableSemantics('http://www.ac.uk');
-
-$wgGroupPermissions['*']	['editproperty'] = false;
-$wgGroupPermissions['sysop']['editproperty'] = true;
-
-############################
-# Bespoke
-############################
-
-
-require_once( 'JBZV/JBZV.php' );
 
 $wgJBZVimagesRootDir        = '/zimages';
 
@@ -190,8 +130,9 @@ $wgJBZVTitleOptions = array(
 							, 'file_name_seperator' => '_'
 );
 
-require_once( 'JBTEIToolbar/JBTEIToolbar.php' );
-require_once( 'TEITags/TEITags.php' );
+require_once( $transcription_extensions_path . 'JBZV/JBZV.php' );
+require_once( $transcription_extensions_path . 'JBTEIToolbar/JBTEIToolbar.php' );
+require_once( $transcription_extensions_path . 'TEITags/TEITags.php' );
 
 
 
