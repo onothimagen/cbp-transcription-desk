6e82bc7a7e8f695b20efd400fc79f6c7110daa9b
Ben Parish <ben@onshop.co.uk>
2014-08-19 04:06:05 +0100

    Improved and simplified LocalSettings.php

--- w/LocalSettings.example.php
+++ w/LocalSettings.example.php
@@ -23,31 +23,36 @@
 * @author Richard Davis <r.davis@ulcc.ac.uk>
 */
 
-$wgScriptPath       = "/w";
-
-#############################################################
 #############################################################
 # CUSTOMISATION SETTINGS
 #############################################################
-#############################################################
+
+$wgScriptPath       = "/w";
 
 #ENABLING PRETTY URLS (N.B HTACCESS ALSO USED
 
 $wgArticlePath = "/td/$1";
 $wgUsePathInfo = true;        # Enable use of pretty URLs
 
+# BP Enabled by default. We need to switch this off because the bentham modern template is not HTML 5
+# HTML Tidy will complain that script tags do not contain a 'type' attribute Html->inlineScript()
+$wgHtml5 					= false;
+
+$wgExternalLinkTarget  = '_blank';
+
+$wgExtensionAssetsPath = $IP . '/extensions/';
+
+require_once( $wgExtensionAssetsPath . '/WikiEditor/WikiEditor.php' );
+
+##############################################################
+# CBP TRANSCRIPTION SKINS
+##############################################################
+
 $wgValidSkinNames[ 'benthammodern' ]            = "BenthamModern";
 $wgValidSkinNames[ 'cbptranscription' ]         = "CbpTranscription";
 $wgValidSkinNames[ 'cbptranscriptionenhanced' ] = "CbpTranscriptionEnhanced";
 
 $wgDefaultSkin = 'cbptranscription';
-
-# BP Enabled by default. We need to switch this off because the bentham modern template is not HTML 5
-# HTML Tidy will complain that script tags do not contain a 'type' attribute Html->inlineScript()
-$wgHtml5 					= false;
-
-$wgExternalLinkTarget = '_blank';
-
 
 $wgLocalStylePath = $IP . '/skins/';
 
@@ -56,79 +61,9 @@ require_once( $wgLocalStylePath . 'CbpTranscription/CbpTranscription.php' );
 require_once( $wgLocalStylePath . 'CbpTranscriptionEnhanced/CbpTranscriptionEnhanced.php' );
 
 
-#############################################################
-# USER PERMISSIONS
-#############################################################
-
-# Disable for everyone.
-$wgGroupPermissions[ '*' ][ 'edit' ]    = false;
-
-# Disable for users, too: by default 'user' is allowed to edit, even if '*' is not.
-$wgGroupPermissions[ 'user' ][ 'edit' ] = false;
-
-# Make it so users with confirmed e-mail addresses are in the group.
-$wgAutopromote[ 'emailconfirmed' ]      = APCOND_EMAILCONFIRMED;
-
-# Hide group from user list.
-$wgImplicitGroups[]                     = 'emailconfirmed';
-
-# Finally, set it to true for the desired group.
-$wgGroupPermissions[ 'emailconfirmed' ][ 'edit' ] 	= true;
-
-#############################################################
-# DEFINE NAMESPACES AND PROTECT FROM EDITING
-#############################################################
-
-
-define("NS_METADATA", 100);
-define("NS_METADATA_TALK", 101);
-
-$wgExtraNamespaces[NS_METADATA]      = 'Metadata';
-$wgExtraNamespaces[NS_METADATA_TALK] = 'Metadata_talk';
-$wgNamespaceProtection[NS_METADATA]  = array( 'editmetadata' ); #permission "editmetaata" required to edit the Metadata namespace
-
-$wgGroupPermissions['sysop']['editmetadata'] = true;      #permission "editmetadata" granted to users in the "sysop" group
-
-#Set default searching
-$wgNamespacesToBeSearchedDefault = array(
-                                        NS_MAIN           => true,
-                                        NS_TALK           => false,
-                                        NS_USER           => false,
-                                        NS_USER_TALK      => false,
-                                        NS_PROJECT        => false,
-                                        NS_PROJECT_TALK   => false,
-                                        NS_IMAGE          => false,
-                                        NS_IMAGE_TALK     => false,
-                                        NS_MEDIAWIKI      => false,
-                                        NS_MEDIAWIKI_TALK => false,
-                                        NS_TEMPLATE       => false,
-                                        NS_TEMPLATE_TALK  => false,
-                                        NS_HELP           => false,
-                                        NS_HELP_TALK      => false,
-                                        NS_CATEGORY       => false,
-                                        NS_CATEGORY_TALK  => false,
-                                        NS_METADATA       => true,
-                                        NS_METADATA_TALK  => false
-                                    );
-
-$wgGroupPermissions[ '*' ]    [ 'edittype' ] = false;
-$wgGroupPermissions[ '*' ]    [ 'edithelp' ] = false;
-$wgGroupPermissions[ 'sysop' ][ 'edittype' ] = true;
-$wgGroupPermissions[ 'sysop' ][ 'edithelp' ] = true;
-$wgGroupPermissions[ 'user' ] [ 'upload' ]   = true;
-
-$wgNamespaceProtection[ NS_HELP ] 	 = array( 'edithelp' );
-$wgNamespacesWithSubpages[ NS_HELP ] = true;
-
-
-$wgNamespaceProtection[NS_HELP]    = array('edithelp');
-$wgNamespacesWithSubpages[NS_HELP] = true;
-
 ##############################################################
 # CBP TRANSCRIPTION EXTENSIONS
 ##############################################################
-
-$wgExtensionAssetsPath = $IP . '/extensions/';
 
 $wgJBZVimagesRootDir   = '/zimages';
 
